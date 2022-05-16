1a3b8e366df08ba17c7f9f04ee005c88ae6d2dd5
Jimmy <jimmycshaw@hotmail.co.nz>
2022-05-16 23:47:16 +1200

    Updated text input fields.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,61 @@ function App() {
             <p>SPEED Database</p>
           </header>
           <div class="submit">
+            <header className="submitTitle">
+              <p>Add Publication</p>
+            </header>
 
+            <form>
+              <label class="textBox">
+                Title:
+                <div></div>
+                <input type="text" class="textField" name="title" />
+              </label>
+              <p></p>
+              <label>
+                Author(s):
+                <div></div>
+                <input type="text" class="textField" name="title" />
+              </label>
+              <p></p>
+              <label>
+                Journal Name:
+                <div></div>
+                <input type="text" class="textField" name="title" />
+              </label>
+              <p></p>
+              <label>
+                Publication Year:
+                <div></div>
+                <input type="text" class="textField" name="title" />
+              </label>
+              <p></p>
+              <label>
+                Volume:
+                <div></div>
+                <input type="text" class="textField" name="title" />
+              </label>
+              <p></p>
+              <label>
+                Number:
+                <div></div>
+                <input type="text" class="textField" name="title" />
+              </label>
+              <p></p>
+              <label>
+                Pages:
+                <div></div>
+                <input type="text" class="textField" name="title" />
+              </label>
+              <p></p>
+              <label>
+                DOI:
+                <div></div>
+                <input type="text" class="textField" name="title" />
+              </label>
+              <p></p>
+              <input type="submit" />
+            </form>
           </div>
         </body>
       </div>
