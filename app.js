3b64fe04f5ffd89baca1c4868c3181026169cea1
Jimmy <jimmycshaw@hotmail.co.nz>
2022-05-19 08:44:58 +1200

    Updated and working Mongo functionality.

---
+++ app.js
@@ -0,0 +1,16 @@
+// app.js
+
+const express = require('express');
+const connectDB = require('./config/db');
+var cors = require('cors');
+
+// routes
+const books = require('./routes/api/books');
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// cors
+app.use(cors({ origin: true, credentials: true }));
