d5f36d78509dbcd4aed457a8635ab3b15f3e4f79
beny098 <83893615+beny098@users.noreply.github.com>
2022-06-01 10:17:00 +1200

    Heroku Changes 2

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const server = express();
-
-server.get('/', (req, res) => {
-    res.send("API is running")
-})
-
-server.listen(5000, console.log("Server is Working and Listening PORT 5000"));
