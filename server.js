d26f6be0d324b1cf90885ce4ce21e3ecc2b9f08f
beny098 <83893615+beny098@users.noreply.github.com>
2022-06-01 10:41:05 +1200

    Revert "heroku changes 5"
    This reverts commit e842c9933f1c35dc6157e06cd053e7717f52be74.

--- server.js
+++ server.js
@@ -7,3 +7,10 @@ server.get('/', (req, res) => {
 })
 
 server.listen(5000, console.log("Server is Working and Listening PORT 5000"));
+
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname + '/client/build/index.html'));
+    });
+}
