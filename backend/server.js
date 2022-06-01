c8d6cfbafd9d215afc072a1bdee80d4d2ab2e623
LiamKing31 <88293543+LiamKing31@users.noreply.github.com>
2022-06-01 16:07:29 +1200

    Functionality of Backend & DB
    Created comments for backend and DB scripts explaining their purpose within the app

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
+/**
+ * @author @JimmyCSH
+ * The following script is a simple backend server
+ * for our SPEED project. Our backend server uses Node.js
+ * to load, request and respond from the server.
+ */
+
 const express = require("express");
 
 const server = express();
 
-server.get('/', (req, res) => {
-    res.send("API is running")
-})
+server.get("/", (req, res) => {
+  res.send("API is running");
+});
 
 server.listen(5000, console.log("Server is Working and Listening PORT 5000"));
