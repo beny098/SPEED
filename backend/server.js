8b2298f7251454386258184972147e08908902bc
beny098 <83893615+beny098@users.noreply.github.com>
2022-06-02 08:36:41 +1200

    Heroku Help

--- backend/server.js
+++ backend/server.js
@@ -7,3 +7,34 @@ server.get('/', (req, res) => {
 })
 
 server.listen(5000, console.log("Server is Working and Listening PORT 5000"));
+
+connectDB();
+server.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+server.use(bodyParser.json());
+server.use(cors({ origin: true, credentials: true }));
+
+// Init Middleware
+server.use(express.json({ extended: false }));
+
+server.use("/api/speed", SPEED);
+
+if (process.env.env === "prod") {
+    server.use(express.static(path.join(__dirname, "frontend/build")));
+
+    // any request that is not caught by the above routes, send back index.html
+    server.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
+    });
+} else {
+    server.get("/", (req, res) => {
+        res.send("Api running!");
+    });
+}
+
+const port = process.env.PORT || 8082;
+
+module.exports = server.listen(port, () =>
+    console.log(`Server running on port ${port}`)
+);
